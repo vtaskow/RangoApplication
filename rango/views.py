2a195911b278b53b958f7002ac608c2e71a4aa96
Viktor Taskov <2209951T@student.gla.ac.uk>
2017-01-16 03:06:51 +0000

    Added media files and templates

--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage' : "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    context_dict = {'author' : "Viktor Taskov"}
+    return render(request, 'rango/about.html', context=context_dict)
