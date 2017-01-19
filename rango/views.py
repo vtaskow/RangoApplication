fb63d4bca23f316b7cb9641f6948983d5f5c8242
Viktor Taskov <2209951T@student.gla.ac.uk>
2017-01-19 22:09:09 +0000

    Adding files

---
+++ rango/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    context_dict = {'boldmessage' : "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    context_dict = {'author' : "Viktor Taskov"}
+    return render(request, 'rango/about.html', context=context_dict)
