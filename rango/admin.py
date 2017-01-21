c261b03559629cdfde0254e7c49eecd625038619
Viktor Taskov <2209951T@student.gla.ac.uk>
2017-01-21 23:48:06 +0000

    Customized rango page admin view

--- rango/admin.py
+++ rango/admin.py
@@ -3,6 +3,9 @@ from .models import Category, Page
 
 # Register your models here.
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ['title','category','url']
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 
